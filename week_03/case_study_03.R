195907c4c8ea38b6050838cb135f8fb6c052b300
Bobby Decker <rjdecker@buffalo.edu>
2022-11-04 20:44:15 -0400

    progress

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,1 +1,17 @@
- 
+#load library
+library(ggplot2) 
+library(dplyr)
+library(gapminder)
+
+#load dataset
+data(gapminder)
+
+#remove Kuwait from gapminder dataset and assign to new variable
+gap_filter <- filter(gapminder, !country == "Kuwait") 
+
+#plot
+ggplot(gap_filter, aes(lifeExp, gdpPercap, color = continent, size=pop/100000)) +
+  geom_point() +
+  facet_wrap(~year,nrow=1) +
+  scale_y_continuous(trans = "sqrt") +
+  labs()
