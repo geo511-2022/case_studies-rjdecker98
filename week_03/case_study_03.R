762f51f279c02519398530743af116a194887820
Bobby Decker <rjdecker@buffalo.edu>
2022-11-05 16:49:40 -0400

    update

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -1,4 +1,4 @@
-#load library
+#load libraries
 library(ggplot2) 
 library(dplyr)
 library(gapminder)
@@ -6,12 +6,34 @@ library(gapminder)
 #load dataset
 data(gapminder)
 
-#remove Kuwait from gapminder dataset and assign to new variable
-gap_filter <- filter(gapminder, !country == "Kuwait") 
+#remove Kuwait from gapminder dataset 
+gap_1 <- gapminder%>%
+  filter(country!="Kuwait")
 
-#plot
-ggplot(gap_filter, aes(lifeExp, gdpPercap, color = continent, size=pop/100000)) +
-  geom_point() +
-  facet_wrap(~year,nrow=1) +
-  scale_y_continuous(trans = "sqrt") +
-  labs()
+#plot 1
+gdp_life<-ggplot(gap_1,aes(lifeExp,gdpPercap,color=continent,size=pop/100000))+
+  geom_point()+
+  facet_wrap(~year,nrow=1)+
+  scale_y_continuous(trans="sqrt")+
+  theme_bw()+
+  labs(
+    title="Wealth and life expectancy through time",
+    x="Life Expectancy",
+    y="GDP per Capita",
+    size="Population(100K)",
+    color="Continent"
+  )
+#print plot
+gdp_life
+
+#create gapminder_continent
+gapminder_continent = gap_1 %>% 
+  group_by(continent, year) %>%
+  summarise(gdpPercapweighted = weighted.mean(x = gdpPercap, w = pop),
+            pop = sum(as.numeric(pop))) 
+
+#plot 2
+gdp_year<-ggplot(gap_1,aes(year,gdpPercap))+
+  geom_line()+
+  geom_point()+
+  geom_line(gapminder_continent,)
