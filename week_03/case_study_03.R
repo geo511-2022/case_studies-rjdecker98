26f60ef4b1bca488534f0295079c2b8524467bfd
Bobby Decker <rjdecker@buffalo.edu>
2022-12-15 23:26:59 -0500

    changes

--- week_03/case_study_03.R
+++ week_03/case_study_03.R
@@ -26,6 +26,11 @@ gdp_life<-ggplot(gap_1,aes(lifeExp,gdpPercap,color=continent,size=pop/100000))+
 #print plot
 gdp_life
 
+#export png
+png("CS03gdp_life.png", width = 15, height = 7, units = "in", res = 300)
+gdp_life
+dev.off()
+
 #create gapminder_continent
 gapminder_continent = gap_1 %>% 
   group_by(continent, year) %>%
@@ -34,6 +39,20 @@ gapminder_continent = gap_1 %>%
 
 #plot 2
 gdp_year<-ggplot(gap_1,aes(year,gdpPercap))+
-  geom_line()+
-  geom_point()+
-  geom_line(gapminder_continent,)
+  geom_line(aes(color=continent,group=country))+
+  geom_point(aes(color=continent,group=country))+
+  geom_line(data=gapminder_continent,aes(x=year,y=gdpPercapweighted))+
+  geom_point(data=gapminder_continent,aes(x=year,y=gdpPercapweighted,size=pop/100000))+
+  facet_wrap(~continent,nrow=1)+
+  theme_bw()+
+  labs(title="GDP Over Time by Continent",
+       x="year",
+       y="GDP per capita",
+       size="Population (100k)",
+       color="Continent")
+gdp_year
+
+#export second png
+png("CS03gdp_year.png", width = 15, height = 7, units = "in", res = 300)
+gdp_year
+dev.off()
